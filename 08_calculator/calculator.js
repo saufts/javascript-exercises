499c95c25d3471d5548db47f02245d29b72a8bdd
saufts <l.szwaczka90@gmail.com>
2023-02-03 00:36:06 +0100

    Finish all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
+const add = function(a,b) {
+  return a + b;
+};
+
+const subtract = function(a,b) {
+  return a - b;
+};
+
+const sum = function(arr) {
+	return arr.reduce((a,b) => a + b, 0); // 0 -> initial value
+};
+
+const multiply = function(arr) {
+  return arr.reduce((a,b) => a * b, 1); // 1 -> initial value
+
+};
+
+const power = function(number, exponent) {
+
+  for(let i =0; i < exponent; i++) {
+    number *= number;
+  }
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(number) {
+	if(number === 0) return 1;
+  return (number * factorial(number - 1));
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toUpperCase().replace(/\W/g, '');
+    let newStr = [...str].reverse().join("");
+    if(newStr === str) return true;
+    else return false;
 };
 
 // Do not edit below this line
