494d1067004a938dca4e810a83fa86a8cfd9f142
saufts <l.szwaczka90@gmail.com>
2023-02-04 14:21:44 +0100

    Finish

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,19 @@
-const findTheOldest = function() {
+
+const findTheOldest = function(ppl) {
+    
+    ppl.forEach (person => {
+        if('yearOfDeath' in person) {
+            person['age'] = person.yearOfDeath - person.yearOfBirth;
+        } else  {
+            const date = new Date();
+            let year = date.getFullYear();
+            person['age'] = year - person.yearOfBirth;
+        };
+    });
+
+    let sortedPeople = ppl.sort((p1, p2) => (p1.age < p2.age) ? 1 : -1);
+    console.log(sortedPeople);
+    return sortedPeople[0];
 
 };
 
